bff176d49a50fddb9669c3e5d3c89a8f9ade34d8
SecondRicecake <2ricecake@naver.com>
2018-12-09 02:01:45 +0900

    Parsing data using strings.exe and grep

---
+++ parseData.py
@@ -0,0 +1,49 @@
+import glob
+import sys
+import re
+import os
+from plumbum import local
+#from plumbum.cmd import grep
+
+
+myPATH = 'D:/Case/UN43NU7150_ChipOff_20181114-20181208T023211Z-001/UN43NU7150_ChipOff_20181114' #input("Folder Path: ")
+
+#print(glob.glob(myPATH+'/*.txt'))
+myFILES =glob.glob(myPATH+'/*.mdf') #List of all mdf files in myPATH folder
+searchPattern =['http','https']
+
+strings = local["C:/Program Files (x86)/GnuWin32/bin/strings.exe"] #loc of my strings.exe
+grep = local["C:/Program Files (x86)/GnuWin32/bin/grep.exe"]
+
+files_count = len(myFILES)
+print('Number of files to parse: ' + str(files_count))
+
+suc_count =0
+err_count =0
+
+for myfile in myFILES: #for each file in the folder
+    fn = os.path.basename(myfile)
+    print("Processing through "+fn+"...")
+    for search in searchPattern: #for each search word in searchPattern
+        command = strings[myfile] | grep[search] #command to cmd
+        s = re.sub(r'[^A-Za-z]+','', search) #strips all non alphabet from search
+        myfName = 'C:/Users/Suz/Desktop/results/'+fn+'_'+s+'.txt'
+        while True:
+            try:
+                (command > myfName)() #execute command; output to [filename][search word].txt 
+                suc_count +=1
+                break
+            except KeyboardInterrupt:
+                raise StopIteration
+            except: #all exceptions
+                err_count +=1          
+                err = str(sys.exc_info()[0])
+                with open(myfName, 'w+') as errf:
+                    errf.write(err +'\nNo Match was found')
+                print ('No Match Found.') 
+                break #break when error
+
+print('Finished. Out of '+str(files_count)+':\n Match Found: '+str(suc_count)+'\n Errors(No Match): '+str(err_count))
+
+
+        
